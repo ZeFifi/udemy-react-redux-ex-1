716f63d756933bb64d9626fc9fa580e925e7f67d
ZeFifi <ph.pinceloup@protonmail.com>
2018-12-03 16:31:45 +0100

    Added state and an event handler (button)

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import UserOutput from "./components/UserOutput/UserOutput";
 
 class App extends Component {
   state = {
-    name: "Philippe"
+    username: "Fifou"
   };
+
+  changeUsernameHandler = () => {
+    this.setState({
+      username: "Best React developer ever"
+    })
+  }
   render() {
     return (
       <div className="App">
         <UserInput />
-        <UserOutput name="Fifou" />
+        <UserOutput name="Yosra" />
         <UserOutput name="Philippe" />
         <UserOutput name="Maxime" />
         <UserOutput name="Thomas" />
+        <UserOutput name={this.state.username} />
+        <button onClick={this.changeUsernameHandler}>Change Fifou's username (state)</button>
       </div>
     );
   }
